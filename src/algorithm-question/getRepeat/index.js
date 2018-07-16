5b6d5c07225ba406c6655c0c9addfc08fcfa7c63
chenqf <qifeng.chen1006@foxmail.com>
2018-07-16 11:04:25 +0800

    获取重复元素

---
+++ src/algorithm-question/getRepeat/index.js
@@ -0,0 +1,9 @@
+/**
+ * 不考虑 NaN 和 稀疏元素的情况下
+ */
+function getRepeat(list) {
+    let result = list.filter((i)=>{
+        return list.indexOf(i) !== list.lastIndexOf(i);
+    })
+    return [...new Set(result)]
+}
