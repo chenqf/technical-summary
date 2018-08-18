848561c8ab9b8bbf8e9cec0fd186aef44f103e09
chenqf <qifeng.chen1006@foxmail.com>
2018-08-18 17:21:43 +0800

    柱状图中最大的矩形

--- src/leetCode/QA/084.largestRectangleArea/README.md
+++ src/leetCode/QA/084.largestRectangleArea/README.md
@@ -16,5 +16,19 @@
 
 ### 解法
 ```javascript 1.8
-
+let largestRectangleArea = function(heights) {
+    let res = 0;
+    let st = [];
+    heights.push(0);
+    for (let i = 0; i < heights.length; ++i) {
+        if (!st.length || heights[st[st.length - 1]] < heights[i]) {
+            st.push(i);
+        } else {
+            let cur = st.pop();
+            res = Math.max(res, heights[cur] * (!st.length ? i : (i - st[st.length - 1] - 1)));
+            --i;
+        }
+    }
+    return res;
+};
 ```
--- src/leetCode/QA/084.largestRectangleArea/index.js
+++ src/leetCode/QA/084.largestRectangleArea/index.js
@@ -1,5 +1,21 @@
 
 
 let largestRectangleArea = function(heights) {
-
+    let res = 0;
+    let st = [];
+    heights.push(0);
+    for (let i = 0; i < heights.length; ++i) {
+        if (!st.length || heights[st[st.length - 1]] < heights[i]) {
+            st.push(i);
+        } else {
+            let cur = st.pop();
+            res = Math.max(res, heights[cur] * (!st.length ? i : (i - st[st.length - 1] - 1) ));
+            --i;
+        }
+    }
+    return res;
 };
+
+
+
+largestRectangleArea([3,6,5,7,4,8,1,0])
