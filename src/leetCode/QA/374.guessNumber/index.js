f1f0af97997295a4bfd6079d792e1615a6d8570b
chenqf <qifeng.chen1006@foxmail.com>
2018-09-12 14:04:59 +0800

    猜数字大小

--- src/leetCode/QA/374.guessNumber/index.js
+++ src/leetCode/QA/374.guessNumber/index.js
@@ -1,22 +1,19 @@
 
-
-
-
 let guessNumber = function (guess) {
     return function (n) {
         if(guess(n) === 0){
             return n;
         }
         let left = 1,right = n;
-        while (left < right){
+        while (left <= right){
             let mid = ~~((left + right)/2);
             let res = guess(mid);
             if(res === 0){
                 return mid;
             }else if(res === 1){
-                left = mid;
+                left = mid + 1;
             }else if(res === -1){
-                right = mid;
+                right = mid - 1;
             }
         }
         return left;
