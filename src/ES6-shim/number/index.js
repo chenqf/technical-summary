49897618bb14b2d71a5e3b50f5683bb379eae38e
chenqf <qifeng.chen1006@foxmail.com>
2018-09-18 16:07:50 +0800

    ES6-shim\number

--- src/ES6-shim/number/index.js
+++ src/ES6-shim/number/index.js
@@ -55,4 +55,12 @@ Number.equal = function ( a , b) {
     return Math.abs((a - b)) <= Number.EPSILON;
 };
 
+/**
+ * 扩展方法 isMinusZero
+ * 判断是否是负零 -0
+ */
+
+Number.isMinusZero = function (num) {
+    return 0 === num && 1/num === -Infinity;
+};
 
