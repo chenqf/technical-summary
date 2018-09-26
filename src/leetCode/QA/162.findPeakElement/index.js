2253875dc53cfdbb36e479a61983ba811564ce62
chenqf <qifeng.chen1006@foxmail.com>
2018-09-26 13:25:23 +0800

    寻找峰值

--- src/leetCode/QA/162.findPeakElement/index.js
+++ src/leetCode/QA/162.findPeakElement/index.js
@@ -1,17 +1,18 @@
 
 
 
-let findPeakElement = function(nums) {
-    if(!nums.length){
-        return -1;
-    }
-    let left = 0,right = nums.length - 1;
-    while (left <= right){
-        let mid = ~~((left + right)/2);
-        if(nums[mid] < nums[mid + 1]){
-            left = mid + 1;
+let findPeakElement = function (nums) {
+    if(!nums || !nums.length) return -1;
+    if(nums.length === 1) return 0;
+    let left = 0,
+        right = nums.length;
+    while (left < right){
+        let mid = ~~(left + (right - left)/2),
+            midVal = nums[mid];
+        if(mid + 1 === nums.length || midVal > nums[mid - 1]){
+            right = mid;
         }else{
-            right = mid - 1;
+            left = mid + 1;
         }
     }
     return left;
