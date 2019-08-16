99876922193f690545f69ebace973fa138741b4b
chenqf <qifeng.chen1006@foxmail.com>
2019-08-16 10:46:50 +0800

    复习

--- docs/.vuepress/components/review.vue
+++ docs/.vuepress/components/review.vue
@@ -1,18 +1,30 @@
 <template>
     <div>
         <br><br>
-        <div v-for="page in this.pages">
-            <a class="cqf_a" v-if="list.indexOf(page.frontmatter.ct) >= 0 || list.indexOf(page.frontmatter.ut) >= 0" @click="toPage(page)">{{page.title}}</a>
+        <div v-for="(page,index) in this.pages">
+            <template v-if="list.indexOf(page.frontmatter.ct) >= 0 || list.indexOf(page.frontmatter.ut) >= 0" >
+                <span v-if="checkBoxList.indexOf(page.path) >= 0" class="over">
+                   已完成
+                </span>
+                <span v-else class="wait">
+                    未完成
+                </span>
+                <a class="cqf_a" @click="toPage(page)">{{page.title}}</a>
+            </template>
+            
         </div>
     </div>    
 </template>
 
 <script>
 
+const CACHE_KEY = 'cqf_cache'
+
 module.exports = {
     props: ['pages'],
     created (){
-        let list = [
+        // 需要复习的日期
+        this.list = [
             moment(new Date()).subtract(1, 'days').format('YYYY/MM/DD'),
             moment(new Date()).subtract(2, 'days').format('YYYY/MM/DD'),
             moment(new Date()).subtract(4, 'days').format('YYYY/MM/DD'),
@@ -20,18 +32,38 @@ module.exports = {
             moment(new Date()).subtract(15, 'days').format('YYYY/MM/DD'),
             moment(new Date()).subtract(30, 'days').format('YYYY/MM/DD'),
             moment(new Date()).subtract(60, 'days').format('YYYY/MM/DD'),
-        ]
-        this.list = list;
+        ]; 
+
+        //找到今天复习的缓存数据
+        let today = moment(new Date()).format('YYYY/MM/DD')
+        let cache = localStorage.getItem(CACHE_KEY) || '{}';
+        let cacheObj = JSON.parse(cache);
+        if(!cacheObj.hasOwnProperty(today)){
+            let valueObj = {}
+            valueObj[today] = [];
+            localStorage.setItem(CACHE_KEY,JSON.stringify(valueObj))
+            cacheObj[today] = [];
+        }
+        this.checkBoxList = cacheObj[today]
+        console.log(this.checkBoxList);
+        
     },
     data:function(){
         return {
-            count:0,
-            list:[]
+            checkBoxList:[],
+            list:[] // 需要复习的日期
         }
     },
     methods:{
-        toPage(page){
-            this.$router.push(page.regularPath)
+        toPage({path,regularPath}){
+            setTimeout(()=>{
+                this.checkBoxList.push(path);
+                let today = moment(new Date()).format('YYYY/MM/DD')
+                let valueObj = {}
+                valueObj[today] = this.checkBoxList;
+                localStorage.setItem(CACHE_KEY,JSON.stringify(valueObj))
+            },500);
+            this.$router.push(regularPath)
         }
     }
 }
@@ -41,6 +73,19 @@ module.exports = {
 <style>
 .cqf_a{
     cursor: pointer;
+}
+
+.over{
+    background-color: #fff5f5!important;
+    color: #ff502c!important;
+    padding: .065em .4em!important;
+    margin-right:5px;
+}
+.wait{
+    margin-right:5px;
+    background-color: #f1eded!important;
+    color:#a09c9b!important;
+    padding: .065em .4em!important;
 }
 
 </style>
